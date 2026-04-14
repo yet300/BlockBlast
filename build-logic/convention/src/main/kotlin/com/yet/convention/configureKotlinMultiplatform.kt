1b31bc76178a3bd7ee97371098a17ac13714b435
yet300 <ghg00rus300@gmail.com>
2026-04-14 21:02:14 +0400

    Add build-logic convention plugin for Kotlin Multiplatform
    - Setup `build-logic` as an included build for shared Gradle conventions.
    - Implement `KotlinMultiplatformPlugin` to centralize KMP configuration.
    - Define shared configuration for Android library, iOS targets, and JDK 17 toolchain.
    - Add common dependencies (coroutines, datetime, serialization) to `commonMain`.
    - Update `libs.versions.toml` with necessary Gradle plugins and version catalog accessors.

---
+++ build-logic/convention/src/main/kotlin/com/yet/convention/configureKotlinMultiplatform.kt
@@ -0,0 +1,52 @@
+import com.android.build.api.dsl.KotlinMultiplatformAndroidLibraryExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
+
+private const val JDK_VERSION = 17
+val JAVA_VERSION = JavaVersion.VERSION_17
+
+
+internal fun Project.configureKotlinMultiplatform(
+    extension: KotlinMultiplatformExtension,
+) = extension.apply {
+    jvmToolchain(JDK_VERSION)
+
+    extensions.configure<KotlinMultiplatformAndroidLibraryExtension>("android") {
+        val moduleName = path.split(":").drop(2).joinToString(".")
+        namespace = if (moduleName.isNotEmpty()) "com.yet.$moduleName" else "com.yet.tetris"
+
+        compileSdk = libs.findVersion("android-compileSdk").get().requiredVersion.toInt()
+        minSdk = libs.findVersion("android-minSdk").get().requiredVersion.toInt()
+
+    }
+
+
+    iosArm64()
+    iosSimulatorArm64()
+
+    applyDefaultHierarchyTemplate()
+
+    configurations.configureEach {
+        if (name.contains("desktop", ignoreCase = true) &&
+            (name.endsWith("CompileClasspath") || name.endsWith("RuntimeClasspath"))
+        ) {
+            attributes.attribute(KotlinPlatformType.attribute, KotlinPlatformType.jvm)
+        }
+    }
+
+    //common dependencies
+    sourceSets.apply {
+        commonMain {
+            dependencies {
+                api(libs.findLibrary("kotlinx-coroutines-core").get())
+                api(libs.findLibrary("kotlinx-datetime").get())
+                api(libs.findLibrary("kotlinx-serialization-json").get())
+            }
+        }
+        commonTest.dependencies {
+            implementation(libs.findBundle("testing").get())
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "BlockBlast"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
         google {
             mavenContent {
