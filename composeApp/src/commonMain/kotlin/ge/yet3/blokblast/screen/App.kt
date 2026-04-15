0acd0815256495e098179c64fa6f39c9cba42ec9
yet300 <ghg00rus300@gmail.com>
2026-04-15 20:08:49 +0400

    Update `App` entry point to use `BlockBlastTheme`

--- composeApp/src/commonMain/kotlin/ge/yet3/blokblast/screen/App.kt
+++ composeApp/src/commonMain/kotlin/ge/yet3/blokblast/screen/App.kt
@@ -1,15 +1,13 @@
 package ge.yet3.blokblast.screen
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import ge.yet.blockblast.feature.root.RootComponent
 import ge.yet3.blokblast.screen.root.RootContent
+import ge.yet3.blokblast.theme.BlockBlastTheme
 
 @Composable
-fun App(
-    rootComponent: RootComponent
-) {
-    MaterialTheme {
+fun App(rootComponent: RootComponent) {
+    BlockBlastTheme {
         RootContent(component = rootComponent)
     }
 }
