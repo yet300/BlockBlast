d2c49f361157decf06344f35d581265691d22d6a
yet300 <ghg00rus300@gmail.com>
2026-05-06 11:26:56 +0400

    Rename application to Logica and increment version code

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "ge.yet.blokblast"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 4
+        versionCode = 5
         versionName = "1.1.2"
     }
 
