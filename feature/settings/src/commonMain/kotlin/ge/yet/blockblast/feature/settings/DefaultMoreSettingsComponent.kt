1f48124cf5e177beb87e2c84831ad85c1fd64cef
yet300 <ghg00rus300@gmail.com>
2026-05-12 19:08:40 +0400

    Refactor `DefaultSettingsComponent` and extract sub-components into standalone files
    - Extract `MainSettingsComponentImpl`, `MoreSettingsComponentImpl`, and `LibrariesSettingsComponentImpl` from `DefaultSettingsComponent` into separate `Default...` classes.
    - Move the `DEFAULT_LIBRARIES` list from a companion object to a new `Libraries.kt` file.
    - Update the navigation logic in `DefaultSettingsComponent` to instantiate the new standalone component implementations.
    - Standardize callback naming by appending `Cb` to internal listener properties.

---
+++ feature/settings/src/commonMain/kotlin/ge/yet/blockblast/feature/settings/DefaultMoreSettingsComponent.kt
@@ -0,0 +1,13 @@
+package ge.yet.blockblast.feature.settings
+
+import com.arkivanov.decompose.ComponentContext
+
+internal class DefaultMoreSettingsComponent(
+    componentContext: ComponentContext,
+    private val onLibrariesClickedCb: () -> Unit,
+    private val onBackClickedCb: () -> Unit,
+) : MoreSettingsComponent, ComponentContext by componentContext {
+
+    override fun onLibrariesClicked() = onLibrariesClickedCb()
+    override fun onBackClicked() = onBackClickedCb()
+}
--- feature/settings/src/commonMain/kotlin/ge/yet/blockblast/feature/settings/DefaultSettingsComponent.kt
+++ feature/settings/src/commonMain/kotlin/ge/yet/blockblast/feature/settings/DefaultSettingsComponent.kt
@@ -1,6 +1,5 @@
 package ge.yet.blockblast.feature.settings
 
-import com.app.common.decompose.asValue
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.router.stack.StackNavigation
@@ -8,11 +7,8 @@ import com.arkivanov.decompose.router.stack.childStack
 import com.arkivanov.decompose.router.stack.pop
 import com.arkivanov.decompose.router.stack.push
 import com.arkivanov.decompose.value.Value
-import com.arkivanov.decompose.value.operator.map
 import com.arkivanov.mvikotlin.core.instancekeeper.getStore
 import dev.zacsweers.metro.Inject
-import ge.yet.blockblast.feature.settings.integration.stateToModel
-import ge.yet.blockblast.feature.settings.store.SettingsStore
 import ge.yet.blockblast.feature.settings.store.SettingsStoreFactory
 import ge.yet.blokblast.domain.repository.AnalyticRepository
 import kotlinx.serialization.Serializable
@@ -49,81 +45,38 @@ internal class DefaultSettingsComponent(
         componentContext: ComponentContext,
     ): SettingsComponent.Child = when (config) {
         Config.Main -> SettingsComponent.Child.Main(
-            MainSettingsComponentImpl(
+            DefaultMainSettingsComponent(
                 componentContext = componentContext,
                 store = store,
-                onMoreClicked = {
-                    analytics.logEvent(eventName = "settings_more_clicked", params = null)
-                    navigation.push(Config.More)
-                },
-                onBackClicked = ::onBackClicked,
+                onMoreClickedCb = ::pushMore,
+                onBackClickedCb = ::onBackClicked,
             )
         )
 
         Config.More -> SettingsComponent.Child.More(
-            MoreSettingsComponentImpl(
+            DefaultMoreSettingsComponent(
                 componentContext = componentContext,
-                onLibrariesClicked = {
-                    analytics.logEvent(eventName = "settings_libraries_clicked", params = null)
-                    navigation.push(Config.Libraries)
-                },
-                onBackClicked = ::onBackClicked,
+                onLibrariesClickedCb = ::pushLibraries,
+                onBackClickedCb = ::onBackClicked,
             )
         )
 
         Config.Libraries -> SettingsComponent.Child.Libraries(
-            LibrariesSettingsComponentImpl(
+            DefaultLibrariesSettingsComponent(
                 componentContext = componentContext,
-                onBackClicked = ::onBackClicked,
+                onBackClickedCb = ::onBackClicked,
             )
         )
     }
 
-    private class MainSettingsComponentImpl(
-        componentContext: ComponentContext,
-        private val store: SettingsStore,
-        private val onMoreClicked: () -> Unit,
-        private val onBackClicked: () -> Unit,
-    ) : MainSettingsComponent, ComponentContext by componentContext {
-
-        override val model: Value<MainSettingsComponent.Model> =
-            store.asValue().map(stateToModel)
-
-        override fun onSoundToggled(enabled: Boolean) {
-            store.accept(SettingsStore.Intent.SetSound(enabled))
-        }
-
-        override fun onVibrationToggled(enabled: Boolean) {
-            store.accept(SettingsStore.Intent.SetVibration(enabled))
-        }
-
-        override fun onDarkThemeToggled(enabled: Boolean) {
-            store.accept(SettingsStore.Intent.SetDark(enabled))
-        }
-
-        override fun onMoreClicked() = onMoreClicked.invoke()
-
-        override fun onBackClicked() = onBackClicked.invoke()
+    private fun pushMore() {
+        analytics.logEvent(eventName = "settings_more_clicked", params = null)
+        navigation.push(Config.More)
     }
 
-    private class MoreSettingsComponentImpl(
-        componentContext: ComponentContext,
-        private val onLibrariesClicked: () -> Unit,
-        private val onBackClicked: () -> Unit,
-    ) : MoreSettingsComponent, ComponentContext by componentContext {
-
-        override fun onLibrariesClicked() = onLibrariesClicked.invoke()
-        override fun onBackClicked() = onBackClicked.invoke()
-    }
-
-    private class LibrariesSettingsComponentImpl(
-        componentContext: ComponentContext,
-        private val onBackClicked: () -> Unit,
-    ) : LibrariesSettingsComponent, ComponentContext by componentContext {
-
-        override val libraries: List<LibrariesSettingsComponent.Library> = DEFAULT_LIBRARIES
-
-        override fun onBackClicked() = onBackClicked.invoke()
+    private fun pushLibraries() {
+        analytics.logEvent(eventName = "settings_libraries_clicked", params = null)
+        navigation.push(Config.Libraries)
     }
 
     @Serializable
@@ -136,86 +89,6 @@ internal class DefaultSettingsComponent(
 
         @Serializable
         data object Libraries : Config
-    }
-
-    private companion object {
-        val DEFAULT_LIBRARIES: List<LibrariesSettingsComponent.Library> = listOf(
-            LibrariesSettingsComponent.Library(
-                name = "Kotlin Multiplatform",
-                description = "Kotlin language and standard library",
-                url = "https://kotlinlang.org/",
-            ),
-            LibrariesSettingsComponent.Library(
-                name = "Compose Multiplatform",
-                description = "JetBrains' cross-platform Compose UI",
-                url = "https://github.com/JetBrains/compose-multiplatform",
-            ),
-            LibrariesSettingsComponent.Library(
-                name = "Material 3",
-                description = "Material Design 3 components for Compose",
-                url = "https://m3.material.io/",
-            ),
-            LibrariesSettingsComponent.Library(
-                name = "Decompose",
-                description = "Lifecycle-aware navigation and components",
-                url = "https://github.com/arkivanov/Decompose",
-            ),
-            LibrariesSettingsComponent.Library(
-                name = "Essenty",
-                description = "Lifecycle, state-keeper, back-handler primitives",
-                url = "https://github.com/arkivanov/Essenty",
-            ),
-            LibrariesSettingsComponent.Library(
-                name = "MVIKotlin",
-                description = "Multiplatform MVI framework",
-                url = "https://github.com/arkivanov/MVIKotlin",
-            ),
-            LibrariesSettingsComponent.Library(
-                name = "Metro",
-                description = "Compile-time dependency injection",
-                url = "https://github.com/ZacSweers/metro",
-            ),
-            LibrariesSettingsComponent.Library(
-                name = "kotlinx.coroutines",
-                description = "Asynchronous programming for Kotlin",
-                url = "https://github.com/Kotlin/kotlinx.coroutines",
-            ),
-            LibrariesSettingsComponent.Library(
-                name = "kotlinx.serialization",
-                description = "Kotlin multiplatform serialization",
-                url = "https://github.com/Kotlin/kotlinx.serialization",
-            ),
-            LibrariesSettingsComponent.Library(
-                name = "kotlinx.datetime",
-                description = "Multiplatform date and time library",
-                url = "https://github.com/Kotlin/kotlinx-datetime",
-            ),
-            LibrariesSettingsComponent.Library(
-                name = "Multiplatform Settings",
-                description = "Key-value storage for KMP",
-                url = "https://github.com/russhwolf/multiplatform-settings",
-            ),
-            LibrariesSettingsComponent.Library(
-                name = "ConfettiKit",
-                description = "Confetti animations for Compose Multiplatform",
-                url = "https://github.com/vinceglb/ConfettiKit",
-            ),
-            LibrariesSettingsComponent.Library(
-                name = "GitLive Firebase",
-                description = "Multiplatform Firebase SDK wrapper",
-                url = "https://github.com/GitLiveApp/firebase-kotlin-sdk",
-            ),
-            LibrariesSettingsComponent.Library(
-                name = "Google Play Services Ads",
-                description = "AdMob SDK for Android",
-                url = "https://developers.google.com/admob/android/quick-start",
-            ),
-            LibrariesSettingsComponent.Library(
-                name = "Google Play In-App Review",
-                description = "Native review prompt on Android",
-                url = "https://developer.android.com/guide/playcore/in-app-review",
-            ),
-        )
     }
 }
 
